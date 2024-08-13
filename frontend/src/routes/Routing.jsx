15c60fa6b487913297eff94a9a46c24dbb58ec7e
= <=>
2024-08-14 02:01:48 +0500

    admin login UI

---
+++ frontend/src/routes/Routing.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import AdminLogin from '../screens/auth/AdminLogin';
+const Routing = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='auth'>
+                    <Route path='admin-login' element={<AdminLogin/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Routing;
