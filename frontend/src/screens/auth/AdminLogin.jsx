0786b4c6b6083c5b0c615a29a313c69699725389
= <=>
2024-08-14 02:12:17 +0500

    admin text changed

--- frontend/src/screens/auth/AdminLogin.jsx
+++ frontend/src/screens/auth/AdminLogin.jsx
@@ -4,7 +4,7 @@ function AdminLogin() {
   return (
     <div className='bg-[#f3f3f3] h-screen flex justify-center items-center'>
         <form className='bg-[#fff] p-3 w-10/12 sm:w-8/12 md:w-6/12 lg:w-3/12 rounded shadow'>
-            <h3 className='mb-4 capitalize font-semibold text-lg'>Dashboard Login</h3>
+            <h3 className='mb-4 capitalize font-semibold text-lg'>Admin Login</h3>
             <div className='mb-4'>
                 <input type='email' name='email' className='w-full p-3 rounded border outline-none' placeholder='Enter Email'/>
             </div>
